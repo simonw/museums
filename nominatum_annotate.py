e303bdd1bb5bcfa5e922ce9a72b83b405ecfab82
Simon Willison <swillison@gmail.com>
2019-11-06 10:07:46 -0800

    Experimental nominatum annotate script
    python nominatum_annotate.py museums.db

---
+++ nominatum_annotate.py
@@ -0,0 +1,27 @@
+import sqlite_utils
+import time
+import requests
+import tqdm
+import json
+
+API_URL = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&extratags=1&namedetails=1&lat={}&lon={}"
+
+
+def annotate(db):
+    for row in tqdm.tqdm(list(db["museums"].rows)):
+        data = requests.get(API_URL.format(row["latitude"], row["longitude"])).json()
+        update = {"osm_{}".format(key): value for key, value in data["address"].items()}
+        update["extratags"] = json.dumps(data["extratags"])
+        update["namedetails"] = json.dumps(data["namedetails"])
+        db["museums"].update(
+            row["id"],
+            update,
+            alter=True,
+        )
+
+
+if __name__ == "__main__":
+    import sys
+
+    db = sqlite_utils.Database(sys.argv[-1])
+    annotate(db)

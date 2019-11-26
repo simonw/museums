7166df7b9c1e6388538d2d804bd700b7b7b9c897
Simon Willison <swillison@gmail.com>
2019-11-26 11:06:57 -0800

    Show press on museum pages, closes #3

---
+++ plugins/template_functions.py
@@ -0,0 +1,11 @@
+from datasette import hookimpl
+import datetime
+import json
+
+
+@hookimpl
+def extra_template_vars():
+    return {
+        "json": json,
+        "nicer_date": lambda d: datetime.datetime.strptime(d, '%d-%M-%Y').strftime("%e %B %Y")
+    }

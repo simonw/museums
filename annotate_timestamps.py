c24895b73b50e75a40c021febe3ecdaaa9dc9b4b
Simon Willison <swillison@gmail.com>
2019-11-28 11:41:19 -0800

    Derive created/updated from git history, closes #4

---
+++ annotate_timestamps.py
@@ -0,0 +1,53 @@
+import git
+import yaml
+import json
+import sqlite_utils
+
+
+IGNORE_CHANGES_IN_COMMITS = {"blah"}
+
+
+def iterate_file_versions(repo_path, filepath, ref="master"):
+    repo = git.Repo(repo_path, odbt=git.GitDB)
+    commits = reversed(list(repo.iter_commits(ref, paths=filepath)))
+    for commit in commits:
+        blob = [b for b in commit.tree.blobs if b.name == filepath][0]
+        yield commit.committed_datetime, commit.hexsha, blob.data_stream.read()
+
+
+if __name__ == "__main__":
+    ref = "master"
+    it = iterate_file_versions(".", "museums.yaml", ref)
+    previous = {}
+    created = {}
+    updated = {}
+    for when, hash, content in it:
+        try:
+            current = {m["id"]: m for m in yaml.safe_load(content)}
+        except yaml.scanner.ScannerError:
+            # This must have been invalid YAML - skip
+            continue
+        # First detect the new museums
+        added_ids = [id for id in current if id not in previous]
+        for id in added_ids:
+            created[id] = when.isoformat()
+            updated[id] = when.isoformat()
+        # Now detect those that have changed since prev
+        if hash not in IGNORE_CHANGES_IN_COMMITS:
+            changed_ids = [
+                id
+                for id in current
+                if json.dumps(current[id], sort_keys=True)
+                != json.dumps(previous.get(id, {}), sort_keys=True)
+            ]
+            for id in changed_ids:
+                updated[id] = when.isoformat()
+        previous = current
+    db = sqlite_utils.Database("browse.db")
+    for id, ts in created.items():
+        db["museums"].update(id, {
+            "created": ts,
+            "updated": updated[id]
+        }, alter=True)
+    print(json.dumps(created, indent=2))
+    print(json.dumps(updated, indent=2))

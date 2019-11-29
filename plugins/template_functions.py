ec303686bcbc32b47bdbf16cf211a8f18a19c666
Simon Willison <swillison@gmail.com>
2019-11-29 12:25:10 -0800

    Fix date parsing again, refs #7

--- plugins/template_functions.py
+++ plugins/template_functions.py
@@ -8,6 +8,6 @@ def extra_template_vars():
     return {
         "json": json,
         "nicer_date": lambda d: "{dt.day} {dt:%B} {dt.year}".format(
-            dt=datetime.datetime.strptime(d, "%Y-%M-%d")
+            dt=datetime.datetime.strptime(d, "%Y-%m-%d")
         ),
     }

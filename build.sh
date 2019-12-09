8d39b7f576858bdfc0e61383b4fed8e37cd0fb57
Simon Willison <swillison@gmail.com>
2019-12-08 17:46:11 -0800

    Normalize osm_country_code to country column, closes #14
    https://www.niche-museums.com/browse/museums?_facet=country

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm -f museums.db
+rm -f browse.db
 sqlite3 about.db ""
 yaml-to-sqlite browse.db museums museums.yaml --pk=id
 python annotate_nominatum.py browse.db
 python annotate_timestamps.py
-sqlite-utils enable-fts browse.db museums name description osm_city osm_country
+sqlite-utils add-column browse.db museums country
+sqlite3 browse.db < set-country.sql
+sqlite-utils enable-fts browse.db museums name description country osm_city

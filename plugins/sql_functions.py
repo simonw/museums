4477306ce8f4913d0912d53ef10b4fd803d2eb89
Simon Willison <swillison@gmail.com>
2019-12-18 09:28:30 +0000

    Render paragraphs in feeds, closes #15

---
+++ plugins/sql_functions.py
@@ -0,0 +1,8 @@
+from datasette import hookimpl
+from datasette_render_markdown import render_markdown
+import json
+
+
+@hookimpl
+def prepare_connection(conn):
+    conn.create_function("render_markdown", 1, render_markdown)

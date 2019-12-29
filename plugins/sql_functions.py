cd3e70a2814d03e4f1d518da55ee588bc7a6ef72
Simon Willison <swillison@gmail.com>
2019-12-29 13:31:44 +0000

    Escape terms in FTS5 queries
    Refs https://github.com/simonw/datasette/issues/651
    
    Searches with commas in no longer break search, e.g.:
    
    https://www.niche-museums.com/browse/search?q=york%2C

--- plugins/sql_functions.py
+++ plugins/sql_functions.py
@@ -1,8 +1,13 @@
 from datasette import hookimpl
 from datasette_render_markdown import render_markdown
-import json
+
+
+def escape_fts_query(query):
+    bits = query.split()
+    return ' '.join('"{}"'.format(bit.replace('"', '')) for bit in bits)
 
 
 @hookimpl
 def prepare_connection(conn):
     conn.create_function("render_markdown", 1, render_markdown)
+    conn.create_function("escape_fts_query", 1, escape_fts_query)

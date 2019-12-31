d8e6f55fa5ac4c80d3d53d664a8755b763430020
Simon Willison <swillison@gmail.com>
2019-12-30 16:30:44 -0800

    Use needs_geocoding column to avoid duplicate geocodes

--- annotate_nominatum.py
+++ annotate_nominatum.py
@@ -8,11 +8,14 @@ API_URL = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&extratags=1
 
 
 def annotate(db):
-    for row in tqdm.tqdm(list(db["museums"].rows_where("country is null"))):
+    if "needs_geocoding" not in db["museums"].columns_dict:
+        db["museums"].add_column("needs_geocoding", bool, not_null_default=1)
+    for row in tqdm.tqdm(list(db["museums"].rows_where("needs_geocoding = 1"))):
         data = requests.get(API_URL.format(row["latitude"], row["longitude"])).json()
         update = {"osm_{}".format(key): value for key, value in data["address"].items()}
         update["extratags"] = json.dumps(data["extratags"])
         update["namedetails"] = json.dumps(data["namedetails"])
+        update["needs_geocoding"] = False
         db["museums"].update(row["id"], update, alter=True)
         time.sleep(1)
 

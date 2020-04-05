29df02f048455ff206112051e35ee7ac3bc52f5e
Simon Willison <swillison@gmail.com>
2020-04-04 19:24:25 -0700

    Redirect niche-museums.com to www.niche-musems.com, refs #21

---
+++ plugins/redirect_www.py
@@ -0,0 +1,46 @@
+from datasette import hookimpl
+from datasette.utils.asgi import asgi_send
+from functools import wraps
+
+HOST_REDIRECTS = {
+    b"nichemuseums.local": b"www.nichemuseums.local",
+    b"niche-museums.com": b"www.niche-museums.com",
+}
+
+
+@hookimpl
+def asgi_wrapper(datasette):
+    def wrap_with_www_redirect(app):
+        @wraps(app)
+        async def www_redirect(scope, recieve, send):
+            if scope["method"] == "GET":
+                host = (dict(scope["headers"]).get(b"host") or b"").split(b":")[0]
+                host_no_port = host.split(b":")[0]
+                if host_no_port in HOST_REDIRECTS:
+                    new_host = HOST_REDIRECTS[host_no_port]
+                    # Is there a port to add?
+                    if b":" in host:
+                        port = host.split(b":")[-1]
+                        new_host += b":" + port
+                    return await send_host_redirect(scope, send, new_host)
+            await app(scope, recieve, send)
+
+        return www_redirect
+
+    return wrap_with_www_redirect
+
+
+async def send_host_redirect(scope, send, new_host, status=301):
+    path = scope.get("raw_path") or scope.get("path", "").encode("utf8")
+    query_string = scope["query_string"]
+    if query_string:
+        path = path + b"?" + query_string
+    new_url = b"https://" + new_host + path
+    await send(
+        {
+            "type": "http.response.start",
+            "status": status,
+            "headers": [[b"location", new_url]],
+        }
+    )
+    await send({"type": "http.response.body", "body": b""})

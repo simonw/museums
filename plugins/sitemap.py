e1ef5319bf0e2f3ebaddc68cb3f749832a62754b
Simon Willison <swillison@gmail.com>
2021-08-09 13:59:00 -0700

    Fixed 500 error on /sitemap.xml

--- plugins/sitemap.py
+++ plugins/sitemap.py
@@ -19,12 +19,12 @@ async def sitemap_xml(datasette):
         '<?xml version="1.0" encoding="UTF-8"?>',
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
     ]
-    for db in datasette.databases.values():
-        for row in await db.execute("select id from museums"):
-            content.append(
-                "<url><loc>https://www.niche-museums.com/{}</loc></url>".format(
-                    row["id"]
-                )
+    db = datasette.get_database("browse")
+    for row in await db.execute("select id from museums"):
+        content.append(
+            "<url><loc>https://www.niche-museums.com/{}</loc></url>".format(
+                row["id"]
             )
+        )
     content.append("</urlset>")
     return Response("\n".join(content), 200, content_type="application/xml")

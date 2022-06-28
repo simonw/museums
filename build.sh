d94410440a5c81a5cb3a0f0b886a8cd30941b8a9
Simon Willison <swillison@gmail.com>
2022-06-27 17:05:35 -0700

    Stemming for FTS plus run build.sh each time, refs #32

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-rm -f browse.db
 yaml-to-sqlite browse.db museums museums.yaml --pk=id
 python annotate_nominatum.py browse.db
 python annotate_timestamps.py
-sqlite-utils add-column browse.db museums country
+# Ignore errors in following block until set -e:
+set +e
+sqlite-utils add-column browse.db museums country 2>/dev/null
 sqlite3 browse.db < set-country.sql
+sqlite-utils disable-fts browse.db museums 2>/dev/null
 sqlite-utils enable-fts browse.db museums \
   name description country osm_city \
-  --tokenize porter
+  --tokenize porter --create-triggers 2>/dev/null
+set -e
